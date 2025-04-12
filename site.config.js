545270bbc06d5c7f3289235fb8ce6b460eb6735f
tertise <awesomeisaac.kim@gmail.com>
2025-04-12 20:19:06 +0900

    site.config.js

--- site.config.js
+++ site.config.js
@@ -1,29 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '1d39a062fa4980eea6dffa9c839efe43',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'The Fanstasy Story',
+  domain: 'fantasy.story.vercel.app',
+  author: 'Isaac Kim',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '저는 fantasy story라는 작은 시리즈를 만들고 있습니다.',
+  socialImageSubtitle: 'Fantasy story 작업실에 오신 걸 환영합니다!!',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // social usernames (optional
+  email : 'awesomeisaac.kim@gmail.com',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'pen_12332960.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
